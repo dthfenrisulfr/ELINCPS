5187963d030a8d4b0de423408057a09920e7bb4b
Станислав <baskervilo88@gmail.com>
2019-11-24 19:45:26 +0600

    1

---
+++ Elin Course Project Service/Services/CPSService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace Elin_Course_Project_Service.Services
+{
+    public class CPSService : ELIN_CPS.ELIN_CPSBase
+    {
+        private readonly ILogger<CPSService> _logger;
+        public CPSService(ILogger<CPSService> logger)
+        {
+            _logger = logger;
+        }
+        public override Task<Staff> GetOneFromStaff(StaffRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new Staff
+            {
+                // Passport = //BDREQUEST;
+                // Name = //BDREQUEST;
+                // SecondName = //BDREQUEST;
+                // MiddleName = //BDREQUEST;
+                // BDate = //BDREQUEST;
+                // Gender = //BDREQUEST;
+                // DateOfReceipt = //BDREQUEST;
+                // Experience = //BDREQUEST;
+            });
+        }
+    }
+}
